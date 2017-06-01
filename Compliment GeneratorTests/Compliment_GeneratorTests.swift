8fa872c4bb1cd4dc699e22aa4bb92fb927cda558
Satwinder Mangat <smangat@administrators-macbook-air.local>
2017-05-31 22:53:03 -0700

    Initial Commit

---
+++ Compliment GeneratorTests/Compliment_GeneratorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Compliment_GeneratorTests.swift
+//  Compliment GeneratorTests
+//
+//  Created by Satwinder Mangat on 5/31/17.
+//  Copyright © 2017 Satwinder Mangat. All rights reserved.
+//
+
+import XCTest
+@testable import Compliment_Generator
+
+class Compliment_GeneratorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
